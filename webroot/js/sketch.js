fbefb97be0a32315cc2ac0bb25344daca422d8c4
Pzybinska <hi@zybinska.io>
2019-07-27 15:17:03 +0200

    radom color function

--- webroot/js/sketch.js
+++ webroot/js/sketch.js
@@ -326,6 +326,10 @@ function setCountryTag(newCountryTag) {
 	generateAnthem(countryname);
 }
 
+function random_rgba() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
 
 async function generateAnthem(seed) {
 	anthem = "";
